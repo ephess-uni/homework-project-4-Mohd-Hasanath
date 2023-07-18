8007e419ac7a54bad38d6e2d1be8ac5c5442ab5a
Mohd Hasanat Hyder <mohd.hasnath@gmail.com>
2023-07-17 20:52:22 -0500

    commit 1

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,15 +8,15 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    modified_date_list = []
+    modi_date_list = []
     for dat in old_dates:
-        modified_date_list.append(datetime.strptime(dat, "%Y-%m-%d").strftime("%d %b %Y"))
-    return modified_date_list
+        modi_date_list.append(datetime.strptime(dat, "%Y-%m-%d").strftime("%d %b %Y"))
+    return modi_date_list
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
-    a list of of `n` datetime objects starting at `start` where each
+    a list of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
     if not isinstance(start, str):
         raise TypeError
@@ -31,23 +31,21 @@ def date_range(start, n):
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
-    `start_date`.  The date, value pairs are returned as tuples
+    `start_date`. The date, value pairs are returned as tuples
     in the returned list."""
 
-    added_list = []
-    for i, elem in enumerate(values):
-        dat_list = []
-        dat_list.append(datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days=i))
-        dat_list.append(elem)
-        added_list.append(tuple(dat_list))
-    return added_list
+    add_list = []
+    for x, elem in enumerate(values):
+        date_lst = []
+        date_lst.append(datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days = x))
+        date_lst.append(elem)
+        add_list.append(tuple(date_lst))
+    return add_list
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-
-
     with open(infile) as file:
         added_list = []
         read_csv_obj = DictReader(file)
@@ -76,11 +74,11 @@ def fees_report(infile, outfile):
                     if len(str(value).split('.')[-1]) != 2:
                         dict[key] = str(value) + "0"
 
-    with open(outfile, "w", newline="") as file:
-        col = ['patron_id', 'late_fees']
-        writer = DictWriter(file, fieldnames=col)
-        writer.writeheader()
-        writer.writerows(updated_list)
+        with open(outfile, "w", newline="") as file:
+            col = ['patron_id', 'late_fees']
+            writer = DictWriter(file, fieldnames=col)
+            writer.writeheader()
+            writer.writerows(updated_list)
 
 
 # The following main selection block will only run when you choose
